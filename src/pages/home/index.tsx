82c3911a0118dee84b1a578f28d78348268163bf
Gilmar Milandri <gilmarmilandri11@gmail.com>
2025-03-10 00:00:25 -0400

    Finalizando

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,20 +1,72 @@
+import { useEffect, useState } from "react"
 import { Social } from "../../components/Social"
 import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa"
+import { db } from "../../services/firebaseConnection"
+import { getDocs, collection, orderBy, query, doc, getDoc  } from "firebase/firestore"
+
 
 export function Home(){
+
+    interface LinkProps {
+        id: string;
+        name: string;
+        url: string;
+        textColor: string;
+        bg: string;
+    }
+
+    interface SocialLinksProps{
+        facebook: string,
+        linkedin: string,
+        instagram: string
+    }
+
+    const [links, setLinks] = useState<LinkProps[]>([]);
+    const [socialLinks, setSociallinks] = useState<SocialLinksProps>()
+
+    useEffect(() => {
+        function loadLinks(){
+            const linksRef = collection(db, "links")
+            const queryRef = query(linksRef, orderBy("created", "asc"))
+
+            getDocs(queryRef)
+            .then((snapshot) => {
+                let lista = [] as LinkProps[];
+
+                snapshot.forEach((doc) =>{
+                    lista.push({
+                        id: doc.id,
+                        name: doc.data().name,
+                        url: doc.data().url,
+                        bg: doc.data().bg,
+                        textColor: doc.data().textColor
+                    })
+                })
+
+                setLinks(lista);
+
+            })
+           
+        }
+
+        loadLinks();
+    }, [])
+
     return (
         <div className="flex flex-col w-full py-4 items-center justify-center">
             <h1 className="md:text-4xl  text-3xl font-bold text-white mt-20">Gilmar Milandri</h1>
             <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
 
             <main className="flex flex-col w-11/12 max-w-xl text-center">
-                <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+                {links.map((Link) =>(
+                    <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
                     <a href="">
                         <p className="text-base md:text-lg ">
                             Canal no youtube
                         </p>
                     </a>
                 </section>
+                ))}
 
                 <footer className="flex justify-center gap-3 my-4">
 
